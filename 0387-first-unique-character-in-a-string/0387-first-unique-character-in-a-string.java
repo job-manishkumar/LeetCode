399ce84bb970f40dd7a56acaf93fb4f9dd0f69b0
manish <118974796+job-manishkumar@users.noreply.github.com>
2025-05-29 19:18:36 +0530

    Time: 6 ms (83.55%), Space: 45 MB (96.59%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -7,12 +7,7 @@ class Solution {
         int[] map = new int[26];
         for(int i=0;i<s.length();i++){
             int index = s.charAt(i) - 'a';
-            if(map[index]==0){
-                map[index] = 1;
-            }
-            else{
-                map[index] = map[index]+1;
-            }
+            map[index] = map[index]+1;
         }
         for(int i=0;i<s.length();i++){
             if(map[s.charAt(i)-'a'] == 1){
