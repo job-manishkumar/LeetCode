c84c0f1cf8317cd707656d78c071584dfd207c4e
manish <118974796+job-manishkumar@users.noreply.github.com>
2025-05-31 15:24:40 +0530

    Time: 2 ms (98.96%), Space: 45.5 MB (71.37%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -5,59 +5,36 @@ class Solution {
     }
 
     public int maxProduct(int[] arr) {
-        int[] ele = getMaxElement(arr);
-        return Math.max(getTwoMinElementProduct(arr) * ele[0], getTwoMaxElementProduct(arr) * ele[1]);
-    }
-
-    public int[] getMaxElement(int[] arr) {
+        int min = Integer.MAX_VALUE;
+        int secondMin = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
-        int second = Integer.MIN_VALUE;
-        int third = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+        int thirdMax = Integer.MIN_VALUE;
 
         for (int i : arr) {
             if (max < i) {
-                third = second;
-                second = max;
+                thirdMax = secondMax;
+                secondMax = max;
                 max = i;
-            } else if (second < i) {
-                third = second;
-                second = i;
-            } else if (third < i) {
-                third = i;
+            }
+            else if (secondMax < i) {
+                thirdMax = secondMax;
+                secondMax = i;
+            }
+            else if(thirdMax< i){
+                thirdMax = i;
+            }
+            if (min > i) {
+                secondMin = min;
+                min = i;
+            } 
+            else if(secondMin > i){
+                    secondMin = i;
             }
         }
-        return new int[] { max, third };
-    }
 
-    public int getTwoMinElementProduct(int[] arr) {
-        for (int i = 0; i < 2; i++) {
-            int min_index = i;
-            for (int j = i; j < arr.length; j++) {
-                if (arr[j] < arr[min_index]) {
-                    min_index = j;
-                }
-            }
-            //swap bwt arr[i] and arr[min_index];
-            int temp = arr[min_index];
-            arr[min_index] = arr[i];
-            arr[i] = temp;
-        }
-        return arr[0] * arr[1];
-    }
-
-    public int getTwoMaxElementProduct(int[] arr) {
-        for (int i = 0; i < 2; i++) {
-            int max_index = i;
-            for (int j = i; j < arr.length; j++) {
-                if (arr[j] > arr[max_index]) {
-                    max_index = j;
-                }
-            }
-            //swap bwt arr[i] and arr[min_index];
-            int temp = arr[max_index];
-            arr[max_index] = arr[i];
-            arr[i] = temp;
-        }
-        return arr[0] * arr[1];
+        long getProdcutWithMin = min*secondMin*max;
+        long getProdcutWithMax = max*secondMax*thirdMax;
+        return (int)Math.max(getProdcutWithMin,getProdcutWithMax);
     }
 }
