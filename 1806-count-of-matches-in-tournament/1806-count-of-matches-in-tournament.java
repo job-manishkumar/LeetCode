615b844764211437053898595c84d50ccd79927c
manish <118974796+job-manishkumar@users.noreply.github.com>
2025-06-09 20:21:34 +0530

    Time: 0 ms (100%), Space: 40.6 MB (39.1%) - LeetHub

--- 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.java
+++ 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.java
@@ -5,17 +5,6 @@ class Solution {
 
     //tc -> O(logn)
     public int getMatches(int n){
-        int sum = 0;
-        while(n>1){
-            if(n%2==0){
-                sum = sum+n/2;
-                n = n/2;
-            }
-            else{
-                sum = sum+ (n-1)/2;
-                n = (n-1)/2+1;
-            }
-        }
-        return sum;
+        return n-1;
     }
 }
