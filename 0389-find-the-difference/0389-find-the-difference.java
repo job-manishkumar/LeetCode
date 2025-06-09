da60d196392359c0d2576a2f7a64efe7843189a0
manish <118974796+job-manishkumar@users.noreply.github.com>
2025-06-09 17:34:49 +0530

    Time: 3 ms (41.41%), Space: 41.7 MB (90.29%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,18 +1,37 @@
 class Solution {
-    public char findTheDifference(String s, String t) {
 
+    public char findTheDifference(String s, String t) {
+        return findDiff2(s, t);
+    }
+
+    public char findDiff(String s, String t) {
+        //apporach tc -> O(n) and sc -> O(1)
         int[] ch = new int[26];
-        for(int i=0;i<t.length();i++){
-            ch[t.charAt(i)-'a']++;
+        for (int i = 0; i < t.length(); i++) {
+            ch[t.charAt(i) - 'a']++;
         }
-        for(int i=0;i<s.length();i++){
-            ch[s.charAt(i)-'a']--;
+        for (int i = 0; i < s.length(); i++) {
+            ch[s.charAt(i) - 'a']--;
         }
-        for(int i=0;i<ch.length;i++){
-            if(ch[i] == 1){
-                return (char)(i+'a');
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i] == 1) {
+                return (char) (i + 'a');
             }
         }
         return '\0';
+    }
+
+    public char findDiff2(String s, String t) {
+        //apporach tc -> O(n) and sc -> O(1)
+        int[] ch = new int[26];
+        int sumS = 0;
+        int sumT = 0;
+        for (int i = 0; i < s.length(); i++) {
+            sumS = sumS + s.charAt(i);
+        }
+        for (int i = 0; i < t.length(); i++) {
+            sumT = sumT + t.charAt(i);
+        }
+        return (char)(sumT - sumS);
     }
 }
