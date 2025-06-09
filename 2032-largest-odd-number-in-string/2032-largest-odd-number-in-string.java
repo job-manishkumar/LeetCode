541443cfd5090f65997e188a008c8e6a5608e6aa
manish <118974796+job-manishkumar@users.noreply.github.com>
2025-06-09 21:49:14 +0530

    Time: 1 ms (99.62%), Space: 45.3 MB (78.68%) - LeetHub

--- 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.java
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.java
@@ -8,12 +8,8 @@ class Solution {
         int index = -1;
         for (int i = num.length() - 1; i >= 0; i--) {
             if (num.charAt(i) % 2 != 0) {
-                index = i;
-                break;
+                return num.substring(0, i + 1);
             }
-        }
-        if (index != -1) {
-            return num.substring(0, index+1);
         }
         return "";
     }
