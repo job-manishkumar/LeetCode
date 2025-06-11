266e4f60dd1e4ddc1a550c0a4f7a6f41d4a4f7e4
manish <118974796+job-manishkumar@users.noreply.github.com>
2025-06-11 13:22:55 +0530

    Time: 28 ms (9.35%), Space: 49.3 MB (9.48%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -5,22 +5,32 @@ class Solution {
     }
 
     public List<List<String>> getGroupAnagrams(String[] strs) {
-        Map<String,List<String>> map = new HashMap<>();
-        for(String s:strs){
-            char[] ch = s.toCharArray();
-            Arrays.sort(ch);
-            String sortString = new String(ch);
-            if(!map.containsKey(sortString)){
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            String sortString = sort(s);
+            if (!map.containsKey(sortString)) {
                 List<String> temp = new ArrayList<>();
-                map.put(sortString,temp);
+                map.put(sortString, temp);
             }
             List<String> list = map.get(sortString);
             list.add(s);
         }
         List<List<String>> ans = new ArrayList<>();
-        for(Map.Entry<String,List<String>> entry:map.entrySet()){
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
             ans.add(entry.getValue());
         }
         return ans;
+    }
+
+    public String sort(String s) {
+        int[] ch = new int[26];
+        for (char c : s.toCharArray()) {
+            ch[c - 'a']++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<ch.length;i++){
+                sb.append(ch[i]+" ");
+        }
+        return sb.toString();
     }
 }
