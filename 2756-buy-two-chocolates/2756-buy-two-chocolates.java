79d8666758b6d59236920242d556a6c7d5837474
manish <118974796+job-manishkumar@users.noreply.github.com>
2025-06-13 21:00:03 +0530

    Time: 1 ms (100%), Space: 44.2 MB (92.12%) - LeetHub

--- 2756-buy-two-chocolates/2756-buy-two-chocolates.java
+++ 2756-buy-two-chocolates/2756-buy-two-chocolates.java
@@ -1,7 +1,7 @@
 class Solution {
 
     public int buyChoco(int[] prices, int money) {
-        return buyChocolates(prices, money);
+        return buyChocolates_2(prices, money);
     }
 
     //approach 1 - - using sorting
@@ -10,5 +10,21 @@ class Solution {
         Arrays.sort(arr);
         int sum = arr[0] + arr[1];
         return sum > money ? money : money - sum;
+    }
+
+    //approach 2 -- using min and secon min
+    public int buyChocolates_2(int[] arr, int money) {
+        int min = Integer.MAX_VALUE;
+        int seconMin = Integer.MAX_VALUE;
+        for (int i : arr) {
+            if (min > i) {
+                seconMin = min;
+                min = i;
+            } else if (seconMin > i) {
+                seconMin = i;
+            }
+        }
+        int sum = min+seconMin;
+        return sum>money?money:money-sum;
     }
 }
