5cb0eb7007c2f7b920879dbffdb2fb32db91d4e1
manish <118974796+job-manishkumar@users.noreply.github.com>
2025-06-13 16:28:24 +0530

    Time: 2 ms (52.37%), Space: 43.4 MB (7.82%) - LeetHub

--- 1574-maximum-product-of-two-elements-in-an-array/1574-maximum-product-of-two-elements-in-an-array.java
+++ 1574-maximum-product-of-two-elements-in-an-array/1574-maximum-product-of-two-elements-in-an-array.java
@@ -1,34 +1,11 @@
 class Solution {
-
     public int maxProduct(int[] nums) {
-        return getMaxProduct(nums);
+        return getProduct(nums);
     }
 
-    public int getMaxProduct(int[] arr) {
-        int max = Integer.MIN_VALUE;
-        int secondMax = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        int secondMin = Integer.MAX_VALUE;
-        for (int i = 0; i < arr.length; i++) {
-            if (max < arr[i]) {
-                secondMax = max;
-                max = arr[i];
-            } else if (secondMax < arr[i]) {
-                secondMax = arr[i];
-            }
-            if (min > arr[i]) {
-                secondMin = min;
-                min = arr[i];
-            } else if (secondMin > arr[i]) {
-                secondMin = arr[i];
-            }
-        }
-        // System.out.println(max+" : "+secondMax+" : "+min+" : "+ secondMin);
-        int multi1 = (min - 1) * (secondMin - 1);
-        int multi2 = (max - 1) * (secondMax - 1);
-        if(multi1>multi2){
-            return multi1;
-        }
-        return multi2;
+    //appraoch 1
+    public int getProduct(int[] arr){
+        Arrays.sort(arr);
+        return (arr[arr.length-1]-1)*(arr[arr.length-2]-1);
     }
 }
