b63b5dc0b8efec4bc7cb58a2f1a4e442a58b18d5
manish <118974796+job-manishkumar@users.noreply.github.com>
2025-06-15 16:45:43 +0530

    Time: 2 ms (92.51%), Space: 43.8 MB (56.17%) - LeetHub

--- 2025-redistribute-characters-to-make-all-strings-equal/2025-redistribute-characters-to-make-all-strings-equal.java
+++ 2025-redistribute-characters-to-make-all-strings-equal/2025-redistribute-characters-to-make-all-strings-equal.java
@@ -1,9 +1,11 @@
 class Solution {
 
     public boolean makeEqual(String[] words) {
-        return isEqual(words);
+        return isEqual_2(words);
     }
 
+    //apprach 1 - using map
+      //tc -> O(n*l) and sc - > O(n)
     public boolean isEqual(String[] words) {
         Map<Character, Integer> map = new HashMap<>();
         for (String word : words) {
@@ -13,6 +15,23 @@ class Solution {
         }
         for (Map.Entry<Character, Integer> entry : map.entrySet()) {
             if (entry.getValue() % words.length != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //approach 2 - using char array
+    //tc -> O(n*l) and sc - > O(1)
+    public boolean isEqual_2(String[] words) {
+        int[] ch = new int[26];
+        for (String word : words) {
+            for (char c : word.toCharArray()) {
+                ch[c - 'a']++;
+            }
+        }
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i] % words.length != 0) {
                 return false;
             }
         }
