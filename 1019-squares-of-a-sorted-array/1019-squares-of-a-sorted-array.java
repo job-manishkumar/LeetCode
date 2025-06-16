636fff641ff854a0782c60a7c657297e877e6c75
manish <118974796+job-manishkumar@users.noreply.github.com>
2025-06-16 12:47:35 +0530

    Time: 1 ms (100%), Space: 47.4 MB (35.12%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -1,9 +1,11 @@
 class Solution {
 
     public int[] sortedSquares(int[] nums) {
-        return getSquares(nums);
+        return getSquares_3(nums);
     }
 
+    //approach 1
+    //tc -> O(nlogn) and sc -> O(1)
     public int[] getSquares(int[] arr) {
         int[] ans = new int[arr.length];
         int index = 0;
@@ -13,6 +15,41 @@ class Solution {
         }
 
         Arrays.sort(ans);
+        return ans;
+    }
+
+    //approach 2 -- using priorityQueue
+    public int[] getSquares_2(int[] arr) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i : arr) {
+            pq.add(i * i);
+        }
+        int[] ans = new int[arr.length];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = pq.poll();
+        }
+        return ans;
+    }
+
+    //approach 3 -- using two pointer
+    public int[] getSquares_3(int[] arr) {
+        int i = 0;
+        int j = arr.length - 1;
+        int[] ans = new int[arr.length];
+        int index = ans.length - 1;
+        while (i <= j) {
+            int squareOfI = arr[i] * arr[i];
+            int squareOfJ = arr[j] * arr[j];
+            if (squareOfI < squareOfJ) {
+                ans[index] = squareOfJ;
+                j--;
+                index--;
+            } else {
+                ans[index] = squareOfI;
+                i++;
+                index--;
+            }
+        }
         return ans;
     }
 }
