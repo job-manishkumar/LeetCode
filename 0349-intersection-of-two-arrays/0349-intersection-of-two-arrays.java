d577991f16fe371b8f68096d154a464f3a130a15
manish <118974796+job-manishkumar@users.noreply.github.com>
2025-06-16 13:36:58 +0530

    Time: 1 ms (98.31%), Space: 43.3 MB (63.2%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,9 +1,11 @@
 class Solution {
 
     public int[] intersection(int[] nums1, int[] nums2) {
-        return getIntersectionElements(nums1, nums2);
+        return getIntersectionElements_2(nums1, nums2);
     }
 
+    //approach 1 - using set
+    //tc -> O(n) and sc -> O(n)
     public int[] getIntersectionElements(int[] arr1, int[] arr2) {
         Set<Integer> set1 = new HashSet<>();
         Set<Integer> set2 = new HashSet<>();
@@ -22,6 +24,30 @@ class Solution {
         int[] ans = new int[list.size()];
         for (int i = 0; i < list.size(); i++) {
             ans[i] = list.get(i);
+        }
+        return ans;
+    }
+
+    //approach 2 - using arr
+    public int[] getIntersectionElements_2(int[] arr1, int[] arr2) {
+        int[] ch = new int[1001];
+        int count = 0;
+        for (int i = 0; i < arr1.length; i++) {
+            ch[arr1[i]]++;
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            if (ch[arr2[i]] > 0) {
+                ch[arr2[i]] = -1;
+                count++;
+            }
+        }
+        int[] ans = new int[count];
+        int index = 0;
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i] == -1) {
+                ans[index] = i;
+                index++;
+            }
         }
         return ans;
     }
