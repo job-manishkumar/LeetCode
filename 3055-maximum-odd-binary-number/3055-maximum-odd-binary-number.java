25f2f374974e2bf8d56e7bf27a13c1cf773f9e49
manish <118974796+job-manishkumar@users.noreply.github.com>
2025-06-16 12:00:42 +0530

    Time: 1 ms (100%), Space: 42.8 MB (99.7%) - LeetHub

--- 3055-maximum-odd-binary-number/3055-maximum-odd-binary-number.java
+++ 3055-maximum-odd-binary-number/3055-maximum-odd-binary-number.java
@@ -1,10 +1,11 @@
 class Solution {
 
     public String maximumOddBinaryNumber(String s) {
-        return maxOddBinary(s);
+        return maxOddBinary_2(s);
     }
 
     //approach 1
+    //tc -> O(n) and sc -> O(n)
     public String maxOddBinary(String s) {
         int count = 0;
         for (int i = 0; i < s.length(); i++) {
@@ -18,10 +19,39 @@ class Solution {
         }
         ch[ch.length - 1] = '1';
         count = count - 1;
-        System.out.println(Arrays.toString(ch));
         for (int i = 0; i < count; i++) {
             ch[i] = '1';
         }
         return new String(ch);
+    }
+
+    //approach 2- using two pointer
+    public String maxOddBinary_2(String s) {
+        int nz = 0;
+        int z = 0;
+        char[] c = s.toCharArray();
+        while (nz < c.length) {
+            if (c[nz] == '1') {
+                //swap ch[nz] and ch[z];
+                char temp = c[nz];
+                c[nz] = c[z];
+                c[z] = temp;
+                z++;
+            }
+            nz++;
+        }
+
+        int index = 0;
+        for (int i = c.length - 1; i >= 0; i--) {
+            if (c[i] == '1') {
+                index = i;
+                break;
+            }
+        }
+        //swap ch[ch.length-1] and ch[index];
+        char temp = c[c.length - 1];
+        c[c.length - 1] = c[index];
+        c[index] = temp;
+        return new String(c);
     }
 }
