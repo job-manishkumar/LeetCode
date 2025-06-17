37cab63548e18c2fd37dab1609b7651fa68f45e6
manish <118974796+job-manishkumar@users.noreply.github.com>
2025-06-17 11:33:58 +0530

    Time: 0 ms (100%), Space: 41.2 MB (73.96%) - LeetHub

--- 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.java
+++ 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.java
@@ -1,11 +1,11 @@
 class Solution {
 
     public int timeRequiredToBuy(int[] tickets, int k) {
-        return timeToBuy(tickets, k);
+        return timeToBuy_2(tickets, k);
     }
 
     //approach 1 -- using queue
-    //tc -> O(n) and sc -> O(n)
+    //tc -> O(n*Max(queueSize)) and sc -> O(n)
     public int timeToBuy(int[] tickets, int k) {
         Queue<Integer> queue = new LinkedList<>();
         int totalTime = 0;
@@ -18,6 +18,20 @@ class Solution {
             totalTime++;
             if (tickets[index] != 0) {
                 queue.add(index);
+            }
+        }
+        return totalTime;
+    }
+
+    //approach 2
+    //tc -> O(n) and sc -> O(1)
+    public int timeToBuy_2(int[] tickets, int k) {
+        int totalTime = 0;
+        for (int i = 0; i < tickets.length; i++) {
+            if (i <= k) {
+                totalTime += Math.min(tickets[i], tickets[k]);
+            } else {
+                totalTime += Math.min(tickets[i], tickets[k] - 1);
             }
         }
         return totalTime;
