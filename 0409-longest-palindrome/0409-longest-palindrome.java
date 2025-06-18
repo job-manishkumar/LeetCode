f3b5989525d33d4d48e64892e10a5c5ba9cec197
manish <118974796+job-manishkumar@users.noreply.github.com>
2025-06-18 10:53:23 +0530

    Time: 2 ms (85.28%), Space: 41.5 MB (95.59%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,22 +1,47 @@
 class Solution {
 
     public int longestPalindrome(String s) {
-        return getLongestPalindrome(s);
+        return getLongestPal_2(s);
     }
 
-    public int getLongestPalindrome(String s) {
-        char[] ch = s.toCharArray();
+    //approach 1
+    //tc -> O(n) and sc -> (n)
+    public int getLongestPal(String s) {
         Map<Character, Integer> map = new HashMap<>();
-        for (char c : ch) {
+        for (char c : s.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
         int count = 0;
         int oneChar = 0;
-        for(Map.Entry<Character,Integer> entry:map.entrySet()) {
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
             if (entry.getValue() % 2 == 0) {
                 count = count + entry.getValue();
             } else {
                 count = count + entry.getValue() - 1;
+                oneChar = 1;
+            }
+        }
+        return count + oneChar;
+    }
+
+    //approach 2
+    //tc -> O(n) and sc -> O(1)
+    public int getLongestPal_2(String s) {
+        int[] ch = new int[52];
+        for (char c : s.toCharArray()) {
+            if (c >= 'A' && c <= 'Z') {
+                ch[c - 'A']++;
+            } else {
+                ch[c - 'a' + 26]++;
+            }
+        }
+        int count = 0;
+        int oneChar = 0;
+        for (int i : ch) {
+            if (i % 2 == 0) {
+                count = count + i;
+            } else {
+                count = count + (i - 1);
                 oneChar = 1;
             }
         }
