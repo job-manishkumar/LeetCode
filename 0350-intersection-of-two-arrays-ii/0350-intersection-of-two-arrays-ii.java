5bacb393b0ff44a8249f68172bc20e5751cd81b4
manish <118974796+job-manishkumar@users.noreply.github.com>
2025-06-19 10:50:01 +0530

    Time: 1 ms (98.61%), Space: 43 MB (85%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,10 +1,11 @@
 class Solution {
 
     public int[] intersect(int[] nums1, int[] nums2) {
-        return intersectArray(nums1, nums2);
+        return intersectArray_2(nums1, nums2);
     }
 
     //approach 1 using Map
+    //tc -> O(n) and sc -> O(n)
     public int[] intersectArray(int[] arr1, int[] arr2) {
         Map<Integer, Integer> map = new HashMap<>();
         List<Integer> list = new ArrayList<>();
@@ -15,6 +16,28 @@ class Solution {
             if (map.containsKey(i) && map.get(i) > 0) {
                 list.add(i);
                 map.put(i, map.get(i) - 1);
+            }
+        }
+
+        int[] ans = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            ans[i] = list.get(i);
+        }
+        return ans;
+    }
+
+    //approach 2 using array
+    //tc -> O(n) and sc -> O(n)
+    public int[] intersectArray_2(int[] arr1, int[] arr2) {
+        int[] ch = new int[1001];
+        List<Integer> list = new ArrayList();
+        for (int i : arr1) {
+            ch[i]++;
+        }
+        for (int i : arr2) {
+            if (ch[i] > 0) {
+                list.add(i);
+                ch[i]--;
             }
         }
 
