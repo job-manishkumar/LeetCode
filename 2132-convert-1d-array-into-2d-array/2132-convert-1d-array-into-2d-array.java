4ad9168fee8482138baaee8aa527272802193880
manish <118974796+job-manishkumar@users.noreply.github.com>
2025-06-20 12:00:32 +0530

    Time: 3 ms (96.2%), Space: 56 MB (10.02%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.java
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.java
@@ -4,21 +4,20 @@ class Solution {
         return get2DArray(original, m, n);
     }
 
+    //approach 1
+    //tc -> O(n2) and sc - > O(n2)
     public int[][] get2DArray(int[] original, int m, int n) {
+        if (original.length != m * n) {
+            return new int[][] {};
+        }
         int[][] ans = new int[m][n];
         int index = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (index == original.length) {
-                    return new int[][] {};
-                }
                 ans[i][j] = original[index];
                 index++;
             }
         }
-        if (index == original.length) {
-            return ans;
-        }
-        return new int[][] {};
+        return ans;
     }
 }
