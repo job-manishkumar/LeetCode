3d100f28b48decc0b72d730d72cf95b729ca7487
manish <118974796+job-manishkumar@users.noreply.github.com>
2025-06-21 11:01:28 +0530

    Time: 26 ms (89.57%), Space: 45.9 MB (27.3%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
@@ -1,11 +1,13 @@
 class Solution {
 
     public String makeFancyString(String s) {
-        return getFancyString(s);
+        return getFancyString_2(s);
     }
 
+    //approach 1
+    //tc -> O(n) and sc O(1)
     public String getFancyString(String s) {
-        if(s.length()<3){
+        if (s.length() < 3) {
             return s;
         }
         char[] ch = s.toCharArray();
@@ -22,6 +24,34 @@ class Solution {
             k++;
         }
         sb = sb.append(ch[i]).append(ch[j]);
+        return sb.toString();
+    }
+
+    //approach 2 -using stack
+    public String getFancyString_2(String s) {
+        if (s.length() < 3) {
+            return s;
+        }
+        char[] st = new char[s.length()];
+        char[] ch = s.toCharArray();
+        int top = 0;
+        for (int i = 0; i < ch.length; i++) {
+            if (top >= 2) {
+                char c1 = st[top - 1];
+                char c2 = st[top - 2];
+                if (!(ch[i] == c1 && ch[i] == c2)) {
+                    st[top] = ch[i];
+                    top++;
+                }
+            } else {
+                st[top] = ch[i];
+                top++;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<top;i++){
+            sb.append(st[i]);
+        }
         return sb.toString();
     }
 }
