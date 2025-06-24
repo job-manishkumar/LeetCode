908fa9f29f78025568fb070b6819fedb2a7317ff
manish <118974796+job-manishkumar@users.noreply.github.com>
2025-06-24 13:15:27 +0530

    Time: 1 ms (100%), Space: 45.4 MB (78.29%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.java
@@ -1,9 +1,11 @@
 class Solution {
 
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        return getMissingAndRepeatedValues(grid);
+        return getMissingAndRepeatedValues_2(grid);
     }
 
+    //approach 1
+    //tc -> O(n2) and sc -> O(n2)
     public int[] getMissingAndRepeatedValues(int[][] mat) {
         int[] ans = new int[2];
         int min = mat.length * mat.length + 1;
@@ -19,6 +21,26 @@ class Solution {
         }
         for (int i = 1; i <= mat.length * mat.length; i++) {
             if (!set.contains(i)) {
+                ans[1] = i;
+            }
+        }
+        return ans;
+    }
+
+    //approach 2
+    public int[] getMissingAndRepeatedValues_2(int[][] mat) {
+        int[] ans = new int[2];
+        int[] count = new int[mat.length * mat.length + 1];
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat.length; j++) {
+                count[mat[i][j]]++;
+            }
+        }
+        for (int i = 1; i < count.length; i++) {
+            if (count[i] == 2) {
+                ans[0] = i;
+            }
+            if (count[i] == 0) {
                 ans[1] = i;
             }
         }
