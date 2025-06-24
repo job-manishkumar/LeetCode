2227b494e5300140c02bb15dfec9f28faf637b8e
manish <118974796+job-manishkumar@users.noreply.github.com>
2025-06-24 12:25:54 +0530

    Time: 5 ms (79.06%), Space: 44.7 MB (71.9%) - LeetHub

--- 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.java
+++ 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.java
@@ -1,9 +1,11 @@
 class Solution {
 
     public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
-        return getKDistantIndices(nums, key, k);
+        return getKDistantIndices_2(nums, key, k);
     }
 
+    //approach1
+    //tc -> O(n2) and sc -> (n)
     public List<Integer> getKDistantIndices(int[] nums, int key, int k) {
         //find all the indices where key is present in nums
         List<Integer> list = new ArrayList<>();
@@ -23,6 +25,33 @@ class Solution {
                     index++;
                 }
             }
+        }
+        return ans;
+    }
+
+    //approach 2
+
+    public List<Integer> getKDistantIndices_2(int[] nums, int key, int k) {
+        //find all the indices where key is present in nums
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (key == nums[i]) {
+                list.add(i);
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        while (i < nums.length && j < list.size()) {
+            if (i - list.get(j) <= k && Math.abs(i - list.get(j)) <= k) {
+                ans.add(i);
+            } else if (i - list.get(j) > k) {
+                j++;
+                if (j < list.size() && Math.abs(i - list.get(j)) <= k) {
+                    ans.add(i);
+                }
+            }
+            i++;
         }
         return ans;
     }
