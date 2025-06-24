c4d39ba05733e316d804899e9cc98431606e16c5
manish <118974796+job-manishkumar@users.noreply.github.com>
2025-06-24 13:32:04 +0530

    Time: 8 ms (46.84%), Space: 44.3 MB (87.23%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
@@ -1,9 +1,11 @@
 class Solution {
 
     public boolean divideArray(int[] nums) {
-        return isDivideArray(nums);
+        return isDivideArray_2(nums);
     }
 
+    //approach 1
+    //tc -> O(n) and sc -> O(1)
     public boolean isDivideArray(int[] arr) {
         int[] count = new int[501];
         for (int i = 0; i < arr.length; i++) {
@@ -11,6 +13,20 @@ class Solution {
         }
         for (int i = 0; i < count.length; i++) {
             if (count[i] % 2 != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //approach 2 -- using map
+    public boolean isDivideArray_2(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() % 2 != 0) {
                 return false;
             }
         }
