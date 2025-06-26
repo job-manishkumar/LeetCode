695ac781bd98eee842c8dc77d2f417142b1b1774
manish <118974796+job-manishkumar@users.noreply.github.com>
2025-06-26 15:53:05 +0530

    Time: 10 ms (30.44%), Space: 44.9 MB (38.37%) - LeetHub

--- 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.java
+++ 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.java
@@ -1,57 +1,25 @@
 class Solution {
 
     public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
-        return getKDistantIndices_2(nums, key, k);
+        return getFindKDistantIndices(nums, key, k);
     }
 
-    //approach1
-    //tc -> O(n2) and sc -> (n)
-    public List<Integer> getKDistantIndices(int[] nums, int key, int k) {
-        //find all the indices where key is present in nums
+    //appraoch 1
+    public List<Integer> getFindKDistantIndices(int[] arr, int key, int k) {
         List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (key == nums[i]) {
+        for (int i = 0; i < arr.length; i++) { //O{n}
+            if (key == arr[i]) {
                 list.add(i);
             }
         }
         List<Integer> ans = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            int index = 0;
-            while (index < list.size()) {
-                if (Math.abs(i - list.get(index)) <= k) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < list.size(); j++) {
+                if (Math.abs(i - list.get(j)) <= k) {
                     ans.add(i);
                     break;
-                } else {
-                    index++;
                 }
             }
-        }
-        return ans;
-    }
-
-    //approach 2
-
-    public List<Integer> getKDistantIndices_2(int[] nums, int key, int k) {
-        //find all the indices where key is present in nums
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (key == nums[i]) {
-                list.add(i);
-            }
-        }
-        List<Integer> ans = new ArrayList<>();
-        int i = 0;
-        int j = 0;
-        while (i < nums.length && j < list.size()) {
-            if (i - list.get(j) <= k && Math.abs(i - list.get(j)) <= k) {
-                ans.add(i);
-            } else if (i - list.get(j) > k) {
-                j++;
-                if (j < list.size() && Math.abs(i - list.get(j)) <= k) {
-                    ans.add(i);
-                }
-            }
-            i++;
         }
         return ans;
     }
