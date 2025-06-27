9da62a071fe3085362b57cf4c41ce4e0b7b6ff00
manish <118974796+job-manishkumar@users.noreply.github.com>
2025-06-27 11:21:47 +0530

    Time: 0 ms (100%), Space: 41.8 MB (29.73%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -1,9 +1,11 @@
 class Solution {
 
     public boolean checkIfPangram(String sentence) {
-        return getCheckIfPangram(sentence);
+        return getCheckIfPangram_2(sentence);
     }
 
+    //aproach 1 -- using char array
+    //tc -> O(n) and sc -> O(1)
     public boolean getCheckIfPangram(String str) {
         int[] ch = new int[26];
         for (char c : str.toCharArray()) {
@@ -11,6 +13,17 @@ class Solution {
         }
         for (int i = 0; i < ch.length; i++) {
             if (ch[i] == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //appraoch 2
+    public boolean getCheckIfPangram_2(String str) {
+        char[] letters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        for (int i = 0; i < letters.length; i++) {
+            if (str.indexOf(letters[i]) == -1) {
                 return false;
             }
         }
