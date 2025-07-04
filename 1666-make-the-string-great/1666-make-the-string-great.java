3647855b5a3ad8935a9d56d461580e0ec331d0a7
manish <118974796+job-manishkumar@users.noreply.github.com>
2025-07-04 11:36:33 +0530

    Time: 1 ms (99.74%), Space: 42.5 MB (39.97%) - LeetHub

---
+++ 1666-make-the-string-great/1666-make-the-string-great.java
@@ -0,0 +1,38 @@
+class Solution {
+
+    public String makeGood(String s) {
+        return getMakeGood(s);
+    }
+
+    public String getMakeGood(String s) {
+        char[] st = new char[s.length()];
+        int top = 0;
+        for (char c : s.toCharArray()) {
+            if (top > 0) {
+                if (c >= 'A' && c <= 'Z') {
+                    if (st[top - 1] >= c + 32 && st[top - 1] <= c + 32) {
+                        top--;
+                    } else {
+                        st[top] = c;
+                        top++;
+                    }
+                } else if (c >= 'a' && c <= 'z') {
+                    if (st[top - 1] >= c - 32 && st[top - 1] <= c - 32) {
+                        top--;
+                    } else {
+                        st[top] = c;
+                        top++;
+                    }
+                }
+            } else {
+                st[top] = c;
+                top++;
+            }
+        }
+        char[] c = new char[top];
+        for (int i = 0; i < c.length; i++) {
+            c[i] = st[i];
+        }
+        return new String(c);
+    }
+}
