d9b8e32b5842ac676815c05abf4c9a53a438f377
manish <118974796+job-manishkumar@users.noreply.github.com>
2025-07-23 12:37:24 +0530

    Time: 0 ms (100%), Space: 41.5 MB (78.9%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -1,29 +1,13 @@
 class Solution {
 
     public boolean checkIfPangram(String sentence) {
-        return getCheckIfPangram_2(sentence);
+        return getCheckIfPangram(sentence);
     }
 
-    //aproach 1 -- using char array
-    //tc -> O(n) and sc -> O(1)
-    public boolean getCheckIfPangram(String str) {
-        int[] ch = new int[26];
-        for (char c : str.toCharArray()) {
-            ch[c - 'a']++;
-        }
-        for (int i = 0; i < ch.length; i++) {
-            if (ch[i] == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    //appraoch 2
-    public boolean getCheckIfPangram_2(String str) {
-        char[] letters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-        for (int i = 0; i < letters.length; i++) {
-            if (str.indexOf(letters[i]) == -1) {
+    public boolean getCheckIfPangram(String sentence) {
+        String alphabets = "abcdefghijklmnopqrstuvwxyz";
+        for (int i = 0; i < 26; i++) {
+            if (sentence.indexOf(alphabets.charAt(i)) == -1) {
                 return false;
             }
         }
