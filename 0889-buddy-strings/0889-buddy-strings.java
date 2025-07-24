dbd629d908648f8cf8df44a130046ce2e9ea4e40
manish <118974796+job-manishkumar@users.noreply.github.com>
2025-07-24 12:40:18 +0530

    Time: 2 ms (74.59%), Space: 42.8 MB (26.67%) - LeetHub

--- 0889-buddy-strings/0889-buddy-strings.java
+++ 0889-buddy-strings/0889-buddy-strings.java
@@ -1,9 +1,11 @@
 class Solution {
 
     public boolean buddyStrings(String s, String goal) {
-        return getBuddyStrings(s, goal);
+        return getBuddyStrings_2(s, goal);
     }
 
+    //approach 1
+    //tc -> O(n) and sc - > O(n)
     public boolean getBuddyStrings(String s, String goal) {
         if (s.length() != goal.length()) {
             return false;
@@ -32,5 +34,42 @@ class Solution {
             return s.charAt(i) == goal.charAt(j) && s.charAt(j) == goal.charAt(i);
         }
         return false;
+    }
+
+    //apporoach 2
+    public boolean getBuddyStrings_2(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        //case 1
+        if (s.equals(goal)) {
+            int[] ch = new int[26];
+            for (char c : s.toCharArray()) {
+                ch[c - 'a']++;
+            }
+            for (int i = 0; i < ch.length; i++) {
+                if (ch[i] != 0 && ch[i] != 1) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        //case 2
+        int[] ch = new int[2];
+        int count = 0;
+        int index = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != goal.charAt(i)) {
+                count++;
+                if (count <= 2) {
+                    ch[index++] = i;
+                } else {
+                    return false;
+                }
+            }
+        }
+        int i = ch[0];
+        int j = ch[1];
+        return s.charAt(i) == goal.charAt(j) && s.charAt(j) == goal.charAt(i);
     }
 }
